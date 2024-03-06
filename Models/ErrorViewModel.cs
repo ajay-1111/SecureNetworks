cb78cd570b2a802074db1b34c426cc8fd3903089
saisrujan44 <saisrujan44@gmail.com>
2024-03-06 16:57:08 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SecureNetworks.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
